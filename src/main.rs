d6afd5a720a8b2c1ffa444a9869bfa090d6c18c1
suraw00t <surawut.kong@gmail.com>
2024-10-22 00:49:14 +0700

    feat: 🎸 add url_for to tera template function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,46 @@
-use actix_web::{middleware, web, App, HttpServer};
+use actix_web::dev::Service;
+use actix_web::{dev::ResourceMap, middleware, test::TestRequest, web, App, HttpServer};
+use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::collections::HashMap;
 
 mod api;
 mod app;
 mod db;
+
+thread_local! {
+    static ROUTES_KEY: RefCell<Option<ResourceMap>> = RefCell::new(None);
+}
+
+fn tera_url_for(args: &HashMap<String, tera::Value>) -> Result<tera::Value, tera::Error> {
+    let name = args["name"]
+        .as_str()
+        .ok_or(tera::Error::msg("`name` should be a string"))?;
+    let empty_elements = tera::Value::Array(vec![]);
+    let elements_iter = args
+        .get("elements")
+        .unwrap_or(&empty_elements)
+        .as_array()
+        .ok_or(tera::Error::msg("`elements` should be an array"))?
+        .iter();
+    let mut elements = vec![];
+    for elem in elements_iter {
+        elements.push(elem.as_str().ok_or(tera::Error::msg(
+            "`elements` array should contain only strings",
+        ))?);
+    }
+    ROUTES_KEY.with(|routes| {
+        let routes_ref = routes.borrow();
+        let routes = routes_ref.as_ref().ok_or(tera::Error::msg(
+            "`url_for` should only be called in request context",
+        ))?;
+        let fake_req = TestRequest::default().to_http_request();
+        let url = routes
+            .url_for(&fake_req, name, elements)
+            .or(Err(tera::Error::msg("resource not found")))?;
+        Ok(tera::Value::String(url.path().to_string()))
+    })
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -15,16 +53,26 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to connect to MongoDB");
 
     log::info!("starting HTTP server at http://localhost:8080");
+    let mut tera = app::initialize_template();
+    tera.register_function("url_for", tera_url_for);
 
     HttpServer::new(move || {
         App::new()
             // enable logger
             .wrap(middleware::Logger::default())
             .app_data(web::JsonConfig::default()) // <- limit size of the payload (global configuration)
-            .app_data(web::Data::new(app::initialize_template()))
+            .app_data(web::Data::new(tera.clone()))
             .app_data(web::Data::new(database.clone()))
             .configure(api::config)
             .configure(app::config)
+            .wrap_fn(move |req, srv| {
+                ROUTES_KEY.with(|routes| {
+                    routes
+                        .borrow_mut()
+                        .get_or_insert_with(|| req.resource_map().clone());
+                });
+                srv.borrow().call(req)
+            })
     })
     .workers(1)
     .bind((std::net::Ipv4Addr::LOCALHOST, 8080))?
