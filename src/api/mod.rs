a7ec1188ca6b9b07a1557e06e5a67cc4360b8c1b
suraw00t <surawut.kong@gmail.com>
2024-10-26 22:54:36 +0700

    feat: 🎸 add support prefix env

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,29 +1,32 @@
 use actix_web::web;
 
-pub mod core;
-mod models;
-mod routes;
-
 use utoipa::OpenApi;
 use utoipa_rapidoc::RapiDoc;
 use utoipa_redoc::{Redoc, Servable};
 use utoipa_scalar::{Scalar, Servable as ScalarServable};
 use utoipa_swagger_ui::SwaggerUi;
 
+pub mod core;
+mod models;
+mod routes;
+
 #[derive(OpenApi)]
 #[openapi(
     nest(
        (path = "/api", api = routes::v1::V1Api)
-    )
+    ),
+    modifiers()
 )]
 struct ApiDoc;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
+    let mut openapi_json = "/api-docs/openapi.json".to_string();
+    if let Some(prefix) = std::env::var("PREFIX").ok() {
+        openapi_json = format!("{}{}", prefix, openapi_json);
+    }
     cfg.service(web::scope("/api").configure(routes::config))
-        .service(
-            SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", ApiDoc::openapi()),
-        )
-        .service(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
+        .service(SwaggerUi::new("/swagger-ui/{_:.*}").url(openapi_json.clone(), ApiDoc::openapi()))
+        .service(RapiDoc::new(openapi_json.clone()).path("/rapidoc"))
         .service(Scalar::with_url("/scalar", ApiDoc::openapi()))
         .service(Redoc::with_url("/redoc", ApiDoc::openapi()));
 }
