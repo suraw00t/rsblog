addf0a20f14409aeb7b568442ae375c7c146e85e
suraw00t <surawut.kong@gmail.com>
2024-10-31 01:51:31 +0700

    fix: 🐛 swagger ui get wrong openapi.json path

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,7 +4,7 @@ use utoipa::{openapi::Server, openapi::ServerBuilder, OpenApi};
 use utoipa_rapidoc::RapiDoc;
 use utoipa_redoc::{Redoc, Servable};
 use utoipa_scalar::{Scalar, Servable as ScalarServable};
-use utoipa_swagger_ui::SwaggerUi;
+use utoipa_swagger_ui::{Config, SwaggerUi};
 
 pub mod core;
 mod models;
@@ -38,13 +38,17 @@ fn get_api_path(base: &str) -> String {
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    let openapi_json = "/api-docs/openapi.json";
+    let openapi_json = get_api_path("/api-docs/openapi.json");
     let mut doc = ApiDoc::openapi();
     doc.servers = Some(get_servers("/"));
 
     cfg.service(web::scope("/api").configure(routes::config))
-        .service(SwaggerUi::new("/swagger-ui/{_:.*}").url(openapi_json, doc.clone()))
-        .service(RapiDoc::new(openapi_json).path("/rapidoc"))
+        .service(
+            SwaggerUi::new("/swagger-ui/{_:.*}")
+                .url("/api-docs/openapi.json", doc.clone())
+                .config(Config::new([openapi_json.clone()])),
+        )
+        .service(RapiDoc::new(openapi_json.clone()).path("/rapidoc"))
         .service(Scalar::with_url("/scalar", doc.clone()))
         .service(Redoc::with_url("/redoc", doc.clone()));
 }
