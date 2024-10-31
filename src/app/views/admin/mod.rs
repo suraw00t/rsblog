de5929d78d8f705b9d69e26a5b790232419f6ab5
suraw00t <surawut.kong@gmail.com>
2024-11-01 00:34:31 +0700

    feat: 🎸 add profile view template

--- src/app/views/admin/mod.rs
+++ src/app/views/admin/mod.rs
@@ -1,7 +1,12 @@
 use actix_web::web;
 
+mod profile;
 mod root;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/admin").configure(root::config));
+    cfg.service(
+        web::scope("/admin")
+            .configure(root::config)
+            .configure(profile::config),
+    );
 }
--- src/app/views/admin/profile.rs
+++ src/app/views/admin/profile.rs
@@ -1,14 +1,14 @@
 use actix_web::{error, web, Error, Responder, Result};
 
 // #[get("/")]
-async fn index(tmpl: web::Data<tera::Tera>) -> Result<impl Responder, Error> {
+async fn profile(tmpl: web::Data<tera::Tera>) -> Result<impl Responder, Error> {
     let html = tmpl
-        .render("admin/index.html", &tera::Context::new())
+        .render("admin/profile.html", &tera::Context::new())
         .map_err(|_| error::ErrorInternalServerError("Template error"))?;
 
     Ok(web::Html::new(html))
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("").name("admin::index").to(index));
+    cfg.service(web::resource("/profile").name("admin::profile").to(profile));
 }
