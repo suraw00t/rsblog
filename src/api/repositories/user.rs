bdff1b3a2cb070fed2242a6680266fa99f5696db
suraw00t <surawut.kong@gmail.com>
2025-06-20 11:52:50 +0700

    feat: 🎸 add disactive and delete

--- src/api/repositories/user.rs
+++ src/api/repositories/user.rs
@@ -1,4 +1,4 @@
-use crate::api::models::user::{CreateUser, FindUser, User};
+use crate::api::models::user::{CreateUser, FindUser, UpdateUser, User};
 use crate::api::repositories::base::BaseRepository;
 
 use mongodb::error::Result;
@@ -18,12 +18,23 @@ impl UserRepository {
         self.base.get(find_user).await
     }
 
-    // Optional: expose base methods directly
     pub async fn create(&self, create_user: CreateUser) -> Result<Option<User>> {
         self.base.create(create_user.into()).await
     }
 
+    pub async fn update(&self, id: String, entity: UpdateUser) -> Result<Option<User>> {
+        self.base.update(id, entity.into()).await
+    }
+
     pub async fn get_by_id(&self, id: String) -> Result<Option<User>> {
         self.base.get_by_id(id).await
+    }
+
+    pub async fn disactive_by_id(&self, id: String) -> Result<Option<User>> {
+        self.base.disactive_by_id(id).await
+    }
+
+    pub async fn delete_by_id(&self, id: String) -> Result<Option<User>> {
+        self.base.delete_by_id(id).await
     }
 }
