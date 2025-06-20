e25490d5d976a67d80e8fb9e784b1ad4a40c61cf
suraw00t <surawut.kong@gmail.com>
2025-06-20 22:44:05 +0700

    feat: 🎸 add service template

---
+++ src/api/services/base.rs
@@ -0,0 +1,11 @@
+pub struct BaseService;
+
+impl BaseService {
+    pub fn get_all();
+    pub fn get_by_id();
+    pub fn get_by_option();
+    pub fn create();
+    pub fn update_by_option();
+    pub fn disactive_by_id();
+    pub fn delete_by_id();
+}
