6ef378e88db5268d7630b01a17f91345cecc11b2
suraw00t <surawut.kong@gmail.com>
2025-06-21 18:19:22 +0700

    refactor: 💡 define gen db uri in config

--- src/api/core/config.rs
+++ src/api/core/config.rs
@@ -2,15 +2,42 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    pub mongodb_uri: String,
-    pub database_name: String,
+    pub db_driver: String,
+    pub db_host: String,
+    pub db_port: String,
+    pub db_name: String,
+    pub db_username: Option<String>,
+    pub db_password: Option<String>,
+    pub db_options: Option<String>,
 }
 
 impl Config {
     pub fn from_env() -> Self {
         Config {
-            mongodb_uri: std::env::var("MONGODB_URI").expect("MONGODB_URI must be set"),
-            database_name: std::env::var("DATABASE_NAME").expect("DATABASE_NAME must be set"),
+            db_driver: std::env::var("DB_DRIVER")
+                .ok()
+                .unwrap_or("mongodb".to_string())
+                .to_lowercase(),
+            db_host: std::env::var("DB_HOST").expect("env `DB_HOST` must be set"),
+            db_port: std::env::var("DB_PORT").ok().unwrap_or("27017".to_string()),
+            db_name: std::env::var("DB_NAME").expect("env `DB_NAME` must be set"),
+            db_username: std::env::var("DB_USERNAME").ok(),
+            db_password: std::env::var("DB_PASSWORD").ok(),
+            db_options: std::env::var("DB_OPTIONS").ok(),
         }
+    }
+
+    pub fn get_database_uri(&self) -> String {
+        let auth_part = match (&self.db_username, &self.db_password) {
+            (Some(user), Some(pass)) => format!("{}:{}@", user, pass),
+            _ => "".to_string(),
+        };
+        let options = self.db_options.clone().unwrap_or("".to_string());
+        let uri = format!(
+            "{}://{}{}:{}/{}{}",
+            self.db_driver, auth_part, self.db_host, self.db_port, self.db_name, options
+        );
+        log::debug!("Database URI: {}", uri);
+        uri
     }
 }
--- src/common/db.rs
+++ src/common/db.rs
@@ -1,19 +1,18 @@
 use crate::api::core::config::Config;
-use mongodb::{Client, Database};
+use mongodb::{options::ClientOptions, Client, Database};
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
 static MONGO_CLIENT: Lazy<Mutex<Option<Database>>> = Lazy::new(|| Mutex::new(None));
 
 pub async fn init_db(config: &Config) {
-    let client = Client::with_uri_str(&config.mongodb_uri)
+    let client_options = ClientOptions::parse(config.get_database_uri())
         .await
-        .expect("MongoDB connect failed");
-    let db = client.database(&config.database_name);
-    log::debug!(
-        "mongodb uri: {}",
-        config.mongodb_uri.clone() + "/" + config.database_name.clone().as_str()
-    );
+        .expect("Failed to parse MongoDB URI");
+    let client = Client::with_options(client_options).expect("MongoDB connect failed");
+    let db = client
+        .default_database()
+        .expect("No default database specified in URI");
     let mut mongo_guard = MONGO_CLIENT.lock().unwrap();
     *mongo_guard = Some(db);
 }
