02ed7ce96dc687fb8cca44ced236a2f7f8a52318
suraw00t <surawut.kong@gmail.com>
2025-06-23 11:35:19 +0700

    refactor: 💡 set default to empty string

--- src/api/schemas/users.rs
+++ src/api/schemas/users.rs
@@ -1,14 +1,16 @@
-use page_hunter::{Book, Page};
 use std::io;
 
-use crate::api::models::users::User;
 use actix_multipart::form::tempfile::TempFile;
 use actix_multipart::form::text::Text;
 use actix_multipart::form::MultipartForm;
 use actix_web::{post, App, HttpServer, Responder};
+
+use page_hunter::{Book, Page};
 use utoipa::ToSchema;
 use utoipa_actix_web::AppExt;
 use utoipa_swagger_ui::SwaggerUi;
+
+use crate::api::models::users::User;
 
 pub type UserPage = Page<User>;
 pub type UserBook = Book<User>;
@@ -18,6 +20,6 @@ pub struct PictureProfile {
     #[multipart(limit = "9mb")]
     #[schema(value_type = String, format = Binary, content_media_type = "application/octet-stream")]
     pub file: TempFile,
-    #[schema(value_type = Option<String>)]
+    #[schema(value_type = Option<String>, default = "")]
     pub name: Option<Text<String>>,
 }
