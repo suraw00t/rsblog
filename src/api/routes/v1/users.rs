25b9ce4fd3d35f3d715550af761c644dd00be549
suraw00t <surawut.kong@gmail.com>
2025-06-25 09:53:48 +0700

    feat: 🎸 add dedicated schema

--- src/api/routes/v1/users.rs
+++ src/api/routes/v1/users.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, put, web, HttpResponse, Responder};
+use actix_web::{get, post, put, web, HttpResponse};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 use mongodb::bson::doc;
 use page_hunter::{bind_records, paginate_records};
@@ -9,13 +9,13 @@ use actix_multipart::form::MultipartForm;
 use crate::api::core::error_handlers;
 use crate::api::models::users::{CreateUser, FindUser, UpdateUser, User};
 use crate::api::repositories;
-use crate::api::schemas::users::{UserBook, UserPage, PictureProfile};
+use crate::api::schemas::users::{UserBook, UserPage, PictureProfile, UploadPictureProfile};
 use crate::api::schemas::{BindingParams, PaginationParams};
 
 #[derive(OpenApi)]
 #[openapi(
     paths(create_user, get_users, get_user, update_user, picture_profile),
-    components(schemas(User, UserBook, UserPage, PictureProfile))
+    // components(schemas(User, UserBook, UserPage, PictureProfile, UploadPictureProfile)),
 )]
 pub struct UserApi;
 
@@ -26,6 +26,7 @@ pub struct UserApi;
     ),
     responses(
         (status = OK, description = "List of users", body = UserPage),
+        (status = UNAUTHORIZED, description = "Unauthorized", body = error_handlers::ErrorResponse),
         (status = UNPROCESSABLE_ENTITY, description = "Unprocessable Entity", body = error_handlers::ErrorResponse),
     ),
     security(),
@@ -35,8 +36,8 @@ pub struct UserApi;
 pub async fn get_users(
     params: web::Query<PaginationParams>,
     find_user: web::Query<FindUser>,
-    // _auth: BearerAuth,
-) -> impl Responder {
+    _auth: BearerAuth,
+) -> Result<HttpResponse, error_handlers::ApiError> {
     let user_repo = repositories::UserRepository::new().await;
     let users = user_repo.get(Some(find_user.into_inner())).await;
     match users {
@@ -80,7 +81,7 @@ async fn get_user(user_id: web::Path<String>) -> Result<HttpResponse, error_hand
     ),
 )]
 #[post("")]
-pub async fn create_user(user_data: web::Json<CreateUser>) -> impl Responder {
+pub async fn create_user(user_data: web::Json<CreateUser>) -> Result<HttpResponse, error_handlers::ApiError> {
     let user_repo = repositories::UserRepository::new().await;
     let user = user_repo.create(user_data.into_inner()).await;
     match user {
@@ -103,7 +104,7 @@ pub async fn create_user(user_data: web::Json<CreateUser>) -> impl Responder {
 pub async fn update_user(
     user_id: web::Path<String>,
     user_data: web::Json<UpdateUser>,
-) -> impl Responder {
+) -> Result<HttpResponse, error_handlers::ApiError> {
     let user_repo = repositories::UserRepository::new().await;
     let user = user_repo
         .update(user_id.to_string(), user_data.into_inner())
@@ -116,27 +117,34 @@ pub async fn update_user(
 }
 
 #[utoipa::path(
-    request_body(content = PictureProfile, content_type = "multipart/form-data")
+    request_body(content = UploadPictureProfile, content_type = "multipart/form-data"),
+    responses(
+        (status = CREATED, description = "Upload user picture profile", body = PictureProfile)
+    ),
 )]
 #[post("/{user_id}/picture")]
-async fn picture_profile(user_id: web::Path<String>, MultipartForm(form): MultipartForm<PictureProfile>) -> impl Responder {
+async fn picture_profile(
+    user_id: web::Path<String>,
+    MultipartForm(form): MultipartForm<UploadPictureProfile>
+) -> Result<HttpResponse, error_handlers::ApiError> {
     log::debug!("User ID: {}", user_id);
-    let name = match form.name.map(|n| n.to_string()) {
-        Some(n) => n,
+    let name = match form.rename.map(|n| n.to_string()) {
+        Some(n) => {
+            n
+        },
         None => "".to_string()
     };
     // let name = form.name.map(|n| n.to_string()).unwrap_or("".to_string());
-    // log::debug!("`name` is empty?: {}", name.is_empty());
     let file = &form.file;
-    format!(
-        "Greetings: name: {name}, type: {} size: {} file_name: {}!",
-        file.content_type
-            .as_ref()
-            .map(|mime| mime.to_string())
-            .unwrap_or_default(),
-        file.size,
-        file.file_name.as_ref().unwrap_or(&String::new())
-    )
+    let picture_profile = serde_json::json!(
+        {
+            "rename": name,
+            "content_type": file.content_type.as_ref().map(|c| c.to_string()).unwrap_or_default(),
+            "size": file.size,
+            "file_name": file.file_name
+        }
+    );
+    Ok(HttpResponse::Created().json(picture_profile))
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
--- src/api/schemas/users.rs
+++ src/api/schemas/users.rs
@@ -1,11 +1,12 @@
 use std::io;
 
+use actix_multipart::form::MultipartForm;
 use actix_multipart::form::tempfile::TempFile;
 use actix_multipart::form::text::Text;
-use actix_multipart::form::MultipartForm;
-use actix_web::{post, App, HttpServer, Responder};
+use actix_web::{App, HttpServer, Responder, post};
 
 use page_hunter::{Book, Page};
+use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use utoipa_actix_web::AppExt;
 use utoipa_swagger_ui::SwaggerUi;
@@ -16,10 +17,18 @@ pub type UserPage = Page<User>;
 pub type UserBook = Book<User>;
 
 #[derive(ToSchema, MultipartForm)]
-pub struct PictureProfile {
-    #[multipart(limit = "9mb")]
+pub struct UploadPictureProfile {
+    #[multipart(limit = "10mb")]
     #[schema(value_type = String, format = Binary, content_media_type = "application/octet-stream")]
     pub file: TempFile,
-    #[schema(value_type = Option<String>, default = "")]
-    pub name: Option<Text<String>>,
+    #[schema(value_type = Option<String>, example = "")]
+    pub rename: Option<Text<String>>,
+}
+
+#[derive(ToSchema, Serialize, Deserialize)]
+pub struct PictureProfile {
+    pub rename: Option<String>,
+    pub size: usize,
+    pub file_name: String,
+    pub content_type: String,
 }
